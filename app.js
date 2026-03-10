c60920f56091a9bf42c7f820710c79b07ab60628
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-03-10 15:29:57 +0000

    added prisma session store

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const expressSession = require("express-session");
+const { PrismaPg } = require('@prisma/adapter-pg');  // For other db adapters, see Prisma docs
+const { PrismaClient } = require('./generated/prisma/client');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const passport = require("passport");
 const assetsPath = path.join(__dirname, "public");
-const indexRouter = require("./routes/indexRouter");
-require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+const indexRouter = require("./routes/indexRouter");
+const pool = require("./db/pool");
+const configurePassport = require("./config/passport");
+require('dotenv').config();
 
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
+
+app.use(
+    expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: 'a santa at nasa',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      prisma,
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+    console.log(req.session);
+    console.log(req.user);
+    next()
+})
+
+configurePassport(passport, pool);
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.use("/", indexRouter)
 
